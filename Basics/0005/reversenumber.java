4189c35449c51e9d92553619422e7b081959f756
Mohammed Ghouse <114724137+GhouseSensei@users.noreply.github.com>
2022-12-15 21:39:45 +0530

    Update reversenumber.java

--- Basics/0005/reversenumber.java
+++ Basics/0005/reversenumber.java
@@ -1,22 +1,10 @@
+//program to reverse user input number
+
 import java.util.*;
 
 public class reversenumber {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        /*
-         * float x = sc.nextFloat();
-         * float n = 0;
-         * for (float i = 10; i <= x * 10; i = i * 10) {
-         * float t = (float) n * 10;
-         * float r = (float) (x % i);
-         * float u = (float) (x % (i / 10));
-         * float v = (float) r - u;
-         * float w = (float) v * (10 / i);
-         * n = (float) (t + w);
-         * }
-         * int k = (int) n;
-         * System.out.println(k);
-         */
         int x = sc.nextInt();
         float n = 0;
         while (x > 0) {
