b626373ecfb2d9157a627727c6bc665f18343862
Mohammed Ghouse <114724137+GhouseSensei@users.noreply.github.com>
2022-12-15 21:20:45 +0530

    Create average.java

---
+++ Basics/average.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class average{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("first number= ");
+        float x = sc.nextFloat();
+        System.out.print("second number= ");
+        float y = sc.nextFloat();
+        System.out.print("third number= ");
+        float z = sc.nextFloat();
+        float avg = (x+y+z)/3;
+        float total = x+y+z;
+        float grndttl = total*(1.18f);
+        System.out.println("total is: " + total);
+        System.out.println("Grand total is: " + grndttl);
+        System.out.println("Average is: " + avg);
+        System.out.println("Side tell: ");
+        float s =sc.nextFloat();
+        float a = s*s;
+        System.out.println("Area is: " + a);
+    }
+}
