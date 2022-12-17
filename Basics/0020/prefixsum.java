3a77279c8d2c8efe5acaea16d771dad90c0f4dae
Mohammed Ghouse <114724137+GhouseSensei@users.noreply.github.com>
2022-12-17 23:48:01 +0530

    Create prefixsum.java

---
+++ Basics/0020/prefixsum.java
@@ -0,0 +1,27 @@
+//program to find max sum of subarray using prefix sum method
+public class subaraprefix {
+    public static void preffixara(int arry[]) {
+        int arry2[] = new int[arry.length];
+        int sumo = 0;
+        int max_sum = Integer.MIN_VALUE;
+        for (int i = 0; i < arry.length; i++) {
+            sumo = sumo + arry[i];
+            arry2[i] = sumo;
+        }
+        for (int j = 0; j < arry.length; j++) {
+            for (int k = j + 1; k < arry.length; k++) {
+                int sum = j == 0 ? arry2[k] : arry2[k] - arry2[j];
+                if (sum > max_sum) {
+                    max_sum = sum;
+                }
+            }
+        }
+        System.out.println(max_sum);
+    }
+
+    public static void main(String args[]) {
+        int arry[] = { 2, 4, 6, 8, 10 };
+        preffixara(arry);
+
+    }
+}
